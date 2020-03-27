68e36a5dfdea59900596f88e1c24417ec37c5de7
TejaswiniSonawane <48352286+TejaswiniSonawane@users.noreply.github.com>
2020-03-27 21:02:37 +0530

    Create image.js

---
+++ image.js
@@ -0,0 +1,9 @@
+const images = {
+    // Image for logo
+    // Logo: require('../images/Logo.png'),
+    // Image for ChatBot Icons
+    // UserIcon: require("../images/user_icon.png"),
+    CPUIcon: require("../images/doctor.png"),
+};
+
+export default images;
